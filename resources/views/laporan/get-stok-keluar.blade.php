3cc8e7430e547dd139ba33223153add51850c5bc
Adi Raka Siwi <adiraka8@gmail.com>
2019-03-17 13:50:08 +0700

    adding year 2019 in filter

--- resources/views/laporan/get-stok-keluar.blade.php
+++ resources/views/laporan/get-stok-keluar.blade.php
@@ -26,6 +26,7 @@
                                         <option value="" disabled selected>-- Pilih Tahun --</option>
                                         <option value="2017">2017</option>
                                         <option value="2018">2018</option>
+                                        <option value="2019">2019</option>
                                     </select>
                                 </div>
                             </div>
@@ -45,6 +46,7 @@
                                         <option value="" disabled selected>-- Pilih Tahun --</option>
                                         <option value="2017">2017</option>
                                         <option value="2018">2018</option>
+                                        <option value="2019">2019</option>
                                     </select>
                                 </div>
                             </div>
